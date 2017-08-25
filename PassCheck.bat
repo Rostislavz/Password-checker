c95992957ea91d9e265f63f09823603cb4e4936c
RZagvo <zgrostislav@gmail.com>
2017-08-25 17:39:44 +0300

    Update PassCheck.bat

--- PassCheck.bat
+++ PassCheck.bat
@@ -4,9 +4,10 @@ echo "new line"
 :Retypepass
 
 set /p passvar1="enter your pass:  "
-set	/p passvar2="confirm your pass:  "
+set /p passvar2="confirm your pass:  "
 
 if not "%passvar1%" == "%passvar2%"  (
+	echo "Pass does not match"
 	echo "retype your pass" 
 	goto Retypepass
 	)
